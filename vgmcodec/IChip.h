31c81738097b688e8c1c70891395a8164742c5ae
leeter <mgetz@leetsoftwerx.com>
2016-02-21 19:27:02 -0700

    get rid of nonstandard macro use, we're targeting VS2015

--- vgmcodec/IChip.h
+++ vgmcodec/IChip.h
@@ -13,9 +13,9 @@ namespace vgmcodec{
 			public:
 				virtual void Write(vgmcodec::sample::VgmSample const & sample) = 0;
 				virtual void Render(gsl::span<vgmcodec::sample::wsample_t> buffer) = 0;
-				virtual std::uint32_t GetDACClock(void) const _NOEXCEPT = 0;
-				virtual std::uint32_t GetSampleRate(void) const _NOEXCEPT = 0;
-				virtual ~IChip() _NOEXCEPT{};
+				virtual std::uint32_t GetDACClock(void) const noexcept = 0;
+				virtual std::uint32_t GetSampleRate(void) const noexcept = 0;
+				virtual ~IChip() noexcept = default;
 			};
 		}
 	}
